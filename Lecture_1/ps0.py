124eb60f2a102135daa3511b754e2c71b555e431
Marin <mdabro85@gmail.com>
2022-05-12 19:00:18 +0200

    :)

---
+++ Lecture_1/ps0.py
@@ -0,0 +1,8 @@
+from cmath import log
+
+
+num = int(input("Enter number x: "));
+num1 = int(input("Enter number y: "));
+power = (num**num1);
+print("x**y =", power);
+print("log(x) =", log(power));
